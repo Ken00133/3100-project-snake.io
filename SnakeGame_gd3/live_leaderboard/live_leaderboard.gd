2b7c0c1d31d714cc1a85277431412f894fc693b2
mnataliew <nataliewong0617@yahoo.com.hk>
2024-03-11 23:32:15 +0800

    add live leaderboard code

---
+++ SnakeGame_gd3/live_leaderboard/live_leaderboard.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	var id_label = get_node("id")
+#	var score_label = get_node("score")
+#	var level_label = get_node("level")
+#	var rank_label = get_node("rank")
+#	var player = get_node("../Snake")
+	
+#	id_label.text = "Snake ID\n" + str(player.player_id)
+#	score_label.text = "Score\n" + str(player.player_score)
+#	level_label.text = "Level\n" + str(player.player_level)
+#	rank_label.text = "Rank\n1"
