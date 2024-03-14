4bbabf5d2db5409df00c904e2aa2cea3db782e1d
Ken00133 <100188567+Ken00133@users.noreply.github.com>
2024-03-14 22:40:20 +0800

    implemented on web!!

--- SnakeGame_gd3/web/front_end_server/src/index.js
+++ SnakeGame_gd3/web/front_end_server/src/index.js
@@ -13,8 +13,8 @@ const App = () => {
       <div>
         {/* Routes Setup */}
         <Routes>
-          <Route path="/" element={<HelloWorld />} />
-          <Route path="/mygame" element={<GameIframe />} />
+          {/*<Route path="/" element={<HelloWorld />} />*/}
+          <Route path="/" element={<GameIframe />} />
         </Routes>
       </div>
     </BrowserRouter>
@@ -27,6 +27,7 @@ const HelloWorld = () => {
 
 const GameIframe = () => {
   return (
+    //<iframe src="https://www.youtube.com/embed/uXWycyeTeCs" width={1000} height={500} title='A youtube video on React hooks'></iframe>
     <iframe src={`${process.env.PUBLIC_URL}/mygame/Snake Game Gd 3.html`} style={{width: '100%', height: '100vh', border: 'none'}} title="Game"></iframe>
   );
 };
