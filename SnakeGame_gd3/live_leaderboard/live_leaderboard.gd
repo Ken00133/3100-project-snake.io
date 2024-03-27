e3d08e7bea8f71de08a27e328de1d0c6367282e6
mnataliew <nataliewong0617@yahoo.com.hk>
2024-03-27 12:11:43 +0800

    update live leaderboard

--- SnakeGame_gd3/live_leaderboard/live_leaderboard.gd
+++ SnakeGame_gd3/live_leaderboard/live_leaderboard.gd
@@ -1,14 +1,11 @@
 extends CanvasLayer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	modify_player_score()
+	update_player_score()
+	update_player_rank(player_rank())
+	update_live_leaderboard()
 
 # get the score and name of player
 func player_score_name():
@@ -17,8 +14,8 @@ func player_score_name():
 	var name = player.get_node("snakeHead").uname
 	return [score, name]
 
-# modify the player score on the left live leaderboard
-func modify_player_score():
+# update the player score on the left live leaderboard
+func update_player_score():
 	var label = get_node("player_score")
 	var score = player_score_name()[0]
 	label.text = "Your score: " + str(score)
@@ -37,22 +34,69 @@ func npc_score_name():
 		score_name.append([score, name])
 	return score_name
 
-func compare_score():
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	var id_label = get_node("id")
-#	var score_label = get_node("score")
-#	var level_label = get_node("level")
-#	var rank_label = get_node("rank")
-#	var player = get_node("../Snake")
+# customize sorting function
+class MyCustomSorter:
+	static func sort_score(a, b):
+		if a[0] > b[0]:
+			return true
+		return false
+
+func sort_NPC():
+	var NPCs_sn = npc_score_name()
+	# sort NPC score name
+	NPCs_sn.sort_custom(MyCustomSorter, "sort_score")
+	return NPCs_sn
 	
-#	id_label.text = "Snake ID\n" + str(player.player_id)
-#	score_label.text = "Score\n" + str(player.player_score)
-#	level_label.text = "Level\n" + str(player.player_level)
-#	rank_label.text = "Rank\n1"
+func player_rank():
+	var player_sn = player_score_name()
+	var NPCs_sn = sort_NPC()
+	var rank = 1
+	for NPC_sn in NPCs_sn:
+		if player_sn[0] > NPC_sn[0]:
+			return rank
+		rank = rank + 1
+	return rank
+
+func update_player_rank(rank):
+	var label = get_node("player_rank")
+	label.text = "Your rank: " + str(rank)
+
+func update_live_leaderboard_label(rank, name, score):
+	var rank_label = get_node("rank")
+	var name_label = get_node("name")
+	var score_label = get_node("score")
+	rank_label.text += "\n" + str(rank)
+	name_label.text += "\n" + name
+	score_label.text += "\n" + str(score)
+	
+func clear_live_leaderboard():
+	var rank_label = get_node("rank")
+	var name_label = get_node("name")
+	var score_label = get_node("score")
+	rank_label.text = "Rank"
+	name_label.text = "Snake Name"
+	score_label.text = "Score"
+	
+func update_live_leaderboard():
+	var NPCs_sn = sort_NPC()
+	var player_sn = player_score_name()
+	var player_r = player_rank()
+	var snake_number = NPCs_sn.size()
+	
+	clear_live_leaderboard()
+	
+	var current_rank = 1
+	for i in range(5):
+		if i > snake_number:
+			return
+		if player_r == i + 1:
+			update_live_leaderboard_label(player_r, player_sn[1], player_sn[0])
+		else:
+			update_live_leaderboard_label(i + 1, NPCs_sn[current_rank - 1][1], NPCs_sn[current_rank - 1][0])
+			current_rank += 1
 
 
 func _on_Timer_timeout():
-	modify_player_score()
-	npc_score_name()
+	update_player_score()
+	update_player_rank(player_rank())
+	update_live_leaderboard()
